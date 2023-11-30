9c63556634f261515b0591e7da1a5d8293e8dda8
adam <athornton@lsst.org>
2023-11-30 16:08:21 -0700

    force some other openssl

--- stage3-py.sh
+++ stage3-py.sh
@@ -31,11 +31,14 @@ mamba install --no-banner -y \
 # source packages use C++ libraries from conda-forge.
 #
 
+mamba install --no-banner -y \
+      "openssl<3.2.0"
+
 pip install --no-build-isolation \
     'jupyter-firefly-extensions>=4.0.0,<5' \
     lsst-rsp \
     git+https://github.com/lsst-sqre/rsp-jupyter-extensions \
-    git+https://github.com/stropy/pyvo.git@7682a1809cb2169cc2ffd9313feb6a4abd7f7e37  # Works with astropy 6 -- remove when pyvo 1.4.3 is out
+    git+https://github.com/astropy/pyvo.git@7682a1809cb2169cc2ffd9313feb6a4abd7f7e37  # Works with astropy 6 -- remove when pyvo 1.4.3 is out
 
 # Add stack kernel
 python3 -m ipykernel install --name 'LSST'
