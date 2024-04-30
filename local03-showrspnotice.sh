2a10fa458e891f18fd1ca0c74b35bf05fed7fbfa
Adam Thornton <athornton@lsst.org>
2024-04-30 13:54:50 -0700

    RFC-992: use lsst_sitcom (not _distrib) in some situations

--- local03-showrspnotice.sh
+++ local03-showrspnotice.sh
@@ -1,14 +1,35 @@
 #!/bin/sh
 
+site_recommendation () {
+    cat << EOF
+
+To create a Rubin Observatory environment in a terminal session and set up
+an appropriate set of packages:
+
+EOF
+
+    case $RSP_SITE_TYPE in
+        telescope | staff)
+            echo "        setup lsst_sitcom"
+	    echo ""
+            ;;
+        *)
+            echo "        setup lsst_distrib"
+	    echo ""
+            ;;
+    esac
+}
+
 case "$-" in
     *i*)
         # OK, we're interactive.
         #  Are we a login shell?
         if shopt -q login_shell; then
-	    # Yes.  Display the notice(s)
+            # Yes.  Display the notice(s)
             if [ -e "/usr/local/etc/rsp_notice" ]; then
                 cat /usr/local/etc/rsp_notice
-	    fi
+                site_recommendation
+            fi
             msgdir="/opt/lsst/software/jupyterlab/messages.d"
             if [ -e  ${msgdir} ]; then
                 any=$(ls ${msgdir})
@@ -16,7 +37,7 @@ case "$-" in
                     cat ${msgdir}/*
                 fi
             fi
-	fi
+        fi
         ;;
     *)
         ;;
--- lsstlaunch.bash
+++ lsstlaunch.bash
@@ -1,19 +1,55 @@
 #!/bin/bash
 CONFIG_FILE=$1
-# /opt/lsst/sal only exists in SAL builds of the stack
-if [ -d /opt/lsst/sal ]; then
-    source /opt/lsst/sal/salbldsteps.bash 2>&1 > /dev/null
-    for i in xml idl sal salobj ATDome ATDomeTrajectory ATMCSSimulator \
-            simactuators standardscripts scriptqueue externalscripts ; do
-        setup ts_${i} -t current
-    done
+
+fallback () {
+    # /opt/lsst/sal only exists in SAL builds of the stack
+    if [ -d /opt/lsst/sal ]; then
+        source /opt/lsst/sal/salbldsteps.bash 2>&1 > /dev/null
+        for i in xml idl sal salobj ATDome ATDomeTrajectory ATMCSSimulator \
+                simactuators standardscripts scriptqueue externalscripts ; do
+            setup ts_${i} -t current
+        done
+	setup lsst_sitcom  # RFC-992
+    else
+        source /opt/lsst/software/stack/loadLSST.bash
+        setup lsst_distrib
+    fi
+    setup display_firefly
+}
+
+if [ -z "${RSP_SITE_TYPE}" ]; then
+   fallback  # Preserve backwards compatibility for a while
 else
-    source /opt/lsst/software/stack/loadLSST.bash
-    setup lsst_distrib
+    case ${RSP_SITE_TYPE} in
+	telescope)
+            source /opt/lsst/sal/salbldsteps.bash 2>&1 > /dev/null
+            for i in xml idl sal salobj ATDome ATDomeTrajectory \
+		     ATMCSSimulator simactuators standardscripts \
+		     scriptqueue externalscripts ; do
+		setup ts_${i} -t current
+            done
+	    for i in lsst_sitcom firefly; do
+		setup $i
+	    done
+	    ;;
+	staff)
+            source /opt/lsst/software/stack/loadLSST.bash
+	    for i in lsst_sitcom firefly; do
+		setup $i
+	    done
+	    ;;
+	*)  # Should be "science", and let's make that the default case.
+            source /opt/lsst/software/stack/loadLSST.bash
+	    for i in lsst_distrib display_firefly ; do
+		setup $i
+	    done
+	    ;;
+    esac
 fi
-setup display_firefly
+
+# Source user_setups if it's there
 if [ -e ${HOME}/notebooks/.user_setups ]; then
     source ${HOME}/notebooks/.user_setups
 fi
+# And now transfer control over to Python
 exec python3 -m ipykernel -f ${CONFIG_FILE}
-
