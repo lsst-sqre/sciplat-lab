44c1ca7414a7551f2c59358d5aad47b143207bd1
adam <athornton@lsst.org>
2024-08-04 09:25:01 -0700

    Add more langpacks, enable CRB

--- stage1-rpm.sh
+++ stage1-rpm.sh
@@ -3,18 +3,20 @@
 # enforcing en_us.utf-8 anymore.
 dnf clean all
 dnf install -y epel-release man man-pages
+# Enable the CRB repository
+/usr/bin/crb enable
 dnf repolist
 dnf -y upgrade
 # Add some other packages
 #  libXScrnSaver ... gtk3 are needed for the chromium installation for
 #   JupyterLab WebPDF conversion
 #  perl-Digest-MD5 ... file are generally useful utilities
-#  glibc-langpack gives us support for many locales
+#  glibc-all-langpacks gives us support for many locales
 #  ...and finally enough editors to cover most people's habits
 dnf -y install \
     libXScrnSaver alsa-lib cups-libs at-spi2-atk pango gtk3 \
     perl-Digest-MD5 jq unzip ack screen tmux tree file \
-    glibc-langpack \
+    glibc-all-langpacks \
     nano vim-enhanced emacs-nox ed
 # Clear build cache
 dnf clean all
