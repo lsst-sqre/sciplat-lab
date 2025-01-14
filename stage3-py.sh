acfd5e1a620ddfd10dd573da738d86770afa4855
Adam Thornton <athornton@lsst.org>
2025-01-14 11:27:25 -0700

    branch for lsst.rsp.startup

--- stage3-py.sh
+++ stage3-py.sh
@@ -48,7 +48,7 @@ pip install uv
 uv pip install --no-build-isolation \
     rsp-jupyter-extensions \
     jupyter-firefly-extensions \
-    'lsst-rsp>=0.5.1' \
+    'git+https://github.com/lsst-sqre/lsst-rsp@tickets-DM-48417' \
     structlog \
     'symbolicmode<3'
 
